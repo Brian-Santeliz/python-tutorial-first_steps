92a3dfb9fcf9274e275610740dbc2324ca726619
Brian <blsvsanteliz@gmail.com>
2019-10-26 18:11:41 -0400

    todo lito

--- functions.py
+++ functions.py
@@ -6,6 +6,9 @@ hola('joe')
 hola()
 
 def add(n1, n2):
-    return n1 * n2
+    return n1 * n2 #retorna el valor de la suma
 
 print(add(20, 30))
+
+
+
---
+++ modules.py
@@ -0,0 +1,7 @@
+from  datetime import timedelta, date
+from fmath import suma, resta #importando modulo creado en local
+print(timedelta(minutes=100))
+print(date.today())
+suma(9,8)
+resta(9, 5)
+
